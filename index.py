8cf936e2939c7a3942f3f5496d491c1da2629ccc
lord2894 <lord2894@gmail.com>
2014-09-03 19:13:32 +0400

    Update and rename index.py to index.html

--- index.py
+++
@@ -1,9 +0,0 @@
-from wsgiref import simple_server
-
-def app(environ, start_response):
-    start_response('200 OK', [('Content-type', 'text/plain')])
-    return ['Hello world!']
-
-server = simple_server.WSGIServer(('', 8000), simple_server.WSGIRequestHandler)
-server.set_app(app)
-server.serve_forever()
